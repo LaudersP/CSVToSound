3e77d55aaee29d31e7133794207e88e37ba942c9
Parker Lauders <parkerwlauders@gmail.com>
2025-03-18 14:21:49 -0400

    feat: Add GUI items and logic

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,5 +8,18 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // Set window size
+            window.Width = 350;
+            window.Height = 430;
+            window.MinimumWidth = 350;
+            window.MinimumHeight = 430;
+
+            return window;
+        }
     }
 }
